4dba8b54d4db58bef0976d86cbd73be73345581d
PhilCAEUser <mail@rwth>
2022-06-26 12:51:22 +0000

    test

--- app/src/main/java/i5/las2peer/services/res/Res.java
+++ app/src/main/java/i5/las2peer/services/res/Res.java
@@ -93,7 +93,7 @@ public class Res extends RESTService {
    * 
    */
   @GET
-  @Path("/test2")
+  @Path("/test")
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.TEXT_PLAIN)
   @ApiResponses(value = {
